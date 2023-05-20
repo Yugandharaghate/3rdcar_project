5ba25019a33c295a194ef665b1ba2dd527257ed7
Yugandharaghate <119990358+Yugandharaghate@users.noreply.github.com>
2023-05-20 12:10:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-CSV_FILE_PATH = r"F:\Yuga Python and Data Science\Practice and Revision\Car_price_pred_flask_project1\artifacts\car_data_dataset.csv"
+CSV_FILE_PATH = r"F:\Car_price_pred_flask_project1\artifacts\car_data_dataset.csv"
 
 MODEL_FILE_PATH = os.path.join("artifacts","linear_reg_model.pkl")
 
